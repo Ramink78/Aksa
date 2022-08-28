9398f1ef90810ecea2c806c7a90a48d9763a0b1d
Ramin Karimi <Raminkarimi1378@gmail.com>
2022-08-28 15:01:55 +0430

    init commit

---
+++ app/src/main/java/rk/aksa/MainActivity.kt
@@ -0,0 +1,11 @@
+package rk.aksa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
